610c5a939d87988b6e4e9d0b19917c589677080f
Nadi Hameza <nadya1gam@gmail.com>
2022-07-11 21:20:35 +0300

    reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+        if (n > 0) {
+            let rev = String(n);
+             let rev1 = Array.from(rev);
+            return rev1.reverse().join('');
+        }
+        else {
+            let min = n * -1;
+            let rev = String(min);
+            let rev1 = Array.from(rev);
+            let massi = rev1.reverse().join('')
+            let numb = Number(massi);
+            return numb;
+        }
 }
